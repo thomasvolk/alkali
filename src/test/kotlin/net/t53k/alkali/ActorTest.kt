476836d85083be63a9bcafa1d1eda2db16e0a213
Thomas Volk <info@thomasvolk.de>
2017-04-16 22:36:43 +0200

    testrunner

--- src/test/kotlin/net/t53k/alkali/ActorTest.kt
+++ src/test/kotlin/net/t53k/alkali/ActorTest.kt
@@ -64,28 +64,42 @@ class PongActor: Actor() {
     }
 }
 
-class ActorTest {
-    private var success = false
-    inner class TestActor: Actor() {
-        override fun receive(message: Any) {
-            when(message) {
-                is Int -> {
-                    assertEquals(message, 99)
-                    success = true
-                    stop()
-                }
-            }
-        }
-
+class TestResultActor(val messageHandler: (Any) -> Unit): Actor() {
+    override fun receive(message: Any) {
+        messageHandler(message)
+        stop()
     }
+}
+
+class ActorTestRunner(val timeout: Long) {
+    fun test(testFun: (ActorSystem) -> Unit) {
+        val system = ActorSystem()
+        try {
+            testFun(system)
+        } finally {
+            Thread.sleep(timeout)
+            system.shutdown()
+            system.waitForShutdown()
+        }
+    }
+}
+
+class ActorTest {
+    private var message: Any? = null
     @Test
     fun pingPong() {
-        val system = ActorSystem()
-        val ping = system.actor("ping", PingActor::class)
-        system.actor("pong", PongActor::class)
-        system.actor("testResult", TestActor())
-        ping.send(Start)
-        system.waitForShutdown()
-        assertTrue(success)
+        val testRunner = ActorTestRunner(2000)
+        testRunner.test { system ->
+            val ping = system.actor("ping", PingActor::class)
+            system.actor("pong", PongActor::class)
+            system.actor("testResult", TestResultActor({ message = it }))
+            ping.send(Start)
+        }
+        when (message) {
+            is Int -> {
+                assertEquals(message, 99)
+            }
+            else -> fail("wrong message: $message")
+        }
     }
 }
